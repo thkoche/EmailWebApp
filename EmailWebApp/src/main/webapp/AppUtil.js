44f4c01347760d671c0757421008b636bff871a0
Koche <thomaskoch96@outlook.com>
2018-06-17 15:16:00 +0200

    Added protoype js; Added login service TODO; Added call for login service;

--- EmailWebApp/src/main/webapp/AppUtil.js
+++ EmailWebApp/src/main/webapp/AppUtil.js
@@ -1,7 +1,9 @@
+/* Set username in header */
 $(document).ready(function() {
 	$('.header-right-text').append(" "+GetUsernameFromURL());
 });
 
+/* returns the used username */
 function GetUsernameFromURL() {
 	var sURLParam = window.location.search.substring(1);
 	var sParameter = sURLParam.split("=");
@@ -9,6 +11,7 @@ function GetUsernameFromURL() {
 	return sParameter[1];
 }
 
+/* print the clicked email */
 $(document).on('click', '.email-container', function() {
     $.ajax({
         url: "http://localhost:8080/EmailWebApp/rest/service/getEmailByUsername/"+GetUsernameFromURL()
@@ -17,3 +20,14 @@ $(document).on('click', '.email-container', function() {
        $('.email-text').append(data.message);
     });
 })
+
+/* reload history (received emails) */
+$(document).on('click', '.refreshBtn', function() {
+	var email = document.createElement("input");
+	var emailtitle = document.createTextNode("This is new");
+	email.appendChild(emailtitle);
+
+	var emaillist = document.getElementById("historylist-emaillist");
+	emaillist.appendChild(email);
+	console.log("Refreshed history");
+});
